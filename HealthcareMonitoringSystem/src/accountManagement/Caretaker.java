c308ad30830051a5abb9229f2268a750d1dc7a18
Karim Hodroj-Remmel <b00073556@aus.edu>
2020-04-13 19:48:42 +0400

    Added the getList functions to Caretaker and Patient java files

--- HealthcareMonitoringSystem/src/accountManagement/Caretaker.java
+++ HealthcareMonitoringSystem/src/accountManagement/Caretaker.java
@@ -1,16 +1,38 @@
 package accountManagement;
+
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import healthcareDatabase.DatabaseCommunication;
+import healthcareDatabase.UserDatabaseCommunication;
 
 public class Caretaker extends User {
     private ArrayList<Patient> patients;
     private boolean hasPermission;
-    
-    public Caretaker(String name, String email, String password, String contact) {
+    private UserDatabaseCommunication db;
+    private boolean patientListChanged;
+
+    public Caretaker(String name, String email, String password, String contact) throws SQLException {
         super(name, email, password, contact);
         patients = new ArrayList<Patient>();
         hasPermission = false;
+        db = new UserDatabaseCommunication("admin", "coe420project");
+        patientListChanged = true;
     }
+    public void getLinkedPatientList() throws SQLException {
+        ArrayList<String[]> patientArray = db.getLinkedPatientList(this.email);
+        Patient user = null;
+        // if the user list has not been changed since the last time the function is called, return
+        if (!patientListChanged)
+            return;
+        for (int i = 0; i < patientArray.size(); i++) {
+            user = new Patient(patientArray.get(i)[0], patientArray.get(i)[1], patientArray.get(i)[2], patientArray.get(i)[3], patientArray.get(i)[5]);
+            patients.add(user);
 
+        }
+            patientListChanged = false;
+        }
+    
 	public boolean doesHavePermission() {
 		return hasPermission;
 	}
--- HealthcareMonitoringSystem/src/accountManagement/Patient.java
+++ HealthcareMonitoringSystem/src/accountManagement/Patient.java
@@ -1,18 +1,40 @@
 package accountManagement;
+
+import java.sql.SQLException;
 import java.util.ArrayList;
-import recordManagement.*;  
+
+import healthcareDatabase.UserDatabaseCommunication;
+import recordManagement.*;
 
 public class Patient extends User {
-    private ArrayList<Record> records;
+	private ArrayList<Record> records;
 	private String emergencyContact;
-	private Caretaker caretakers[];
+	private ArrayList<Caretaker> caretakers;
+	private UserDatabaseCommunication db;
+	private boolean CaretakerListChanged;
 
-    public Patient(String name, String email, String password, String contact, String emergencyContact) {
+	public Patient(String name, String email, String password, String contact, String emergencyContact)
+			throws SQLException {
 		super(name, email, password, contact);
 		this.emergencyContact = emergencyContact;
-        records = new ArrayList<Record>();
+		records = new ArrayList<Record>();
+		caretakers = new ArrayList<Caretaker>();
+		db = new UserDatabaseCommunication("admin", "coe420project");
+        CaretakerListChanged = true;
     }
+    public void getLinkedPatientList() throws SQLException {
+        ArrayList<String[]> CaretakerArray = db.getLinkedCaretakerList(this.email);
+        Caretaker user = null;
+        // if the user list has not been changed since the last time the function is called, return
+        if (!CaretakerListChanged)
+            return;
+        for (int i = 0; i < CaretakerArray.size(); i++) {
+            user = new Caretaker(CaretakerArray.get(i)[0], CaretakerArray.get(i)[1], CaretakerArray.get(i)[2], CaretakerArray.get(i)[3]);
+            caretakers.add(user);
 
+        }
+            CaretakerListChanged = false;
+        }
 	public String getEmergencyContact() {
 		return emergencyContact;
 	}
