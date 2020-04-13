30deb4faa7955db681507abf2c5ae8dbac5f08b7
Karim Hodroj-Remmel <b00073556@aus.edu>
2020-04-13 22:31:27 +0400

    Added getrecordlist func in doctor class

--- HealthcareMonitoringSystem/src/accountManagement/Doctor.java
+++ HealthcareMonitoringSystem/src/accountManagement/Doctor.java
@@ -3,12 +3,15 @@ package accountManagement;
 import recordManagement.*;
 
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import healthcareDatabase.InputErrorException;
 import healthcareDatabase.RecordsDatabaseCommunication;
 import healthcareDatabase.UserNotFoundException;
-
+import recordManagement.*;
 public class Doctor extends User {
     private ArrayList<Record> records;
     private RecordsDatabaseCommunication db;
@@ -18,14 +21,51 @@ public class Doctor extends User {
         super(name, email, password, contact);
         records = new ArrayList<Record>();
     }
+    public Date StringToDate(String s){
 
+        Date result = null;
+        try{
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            result  = dateFormat.parse(s);
+        }
+    
+        catch(ParseException e){
+            e.printStackTrace();
+    
+        }
+        return result;
+    }
+    public void getRecordsList() throws SQLException {
+        ArrayList<String[]> recordArray = db.getRecordList();
+        Record record = null;
+        ArrayList<Date> datesarray;
+        // if the user list has not been changed since the last time the function is called, return
+        if (!recordListChanged)
+            return;
+        for (int i = 0; i < recordArray.size(); i++) {
+            String recordType = recordArray.get(i)[6]; // double-check this
+
+            if (recordType.equals("Prescription")) {
+                datesarray.add(StringToDate( recordArray.get(i)[1]));
+                record = new Prescription(recordArray.get(i)[0], datesarray, recordArray.get(i)[2],Boolean.parseBoolean( recordArray.get(i)[3]), Integer.parseInt(recordArray.get(i)[4]),  Integer.parseInt(recordArray.get(i)[5]));
+            }
+            else if (recordType.equals("Vital")) {
+                datesarray.add(StringToDate( recordArray.get(i)[1]));
+                record = new Vital(recordArray.get(i)[0], datesarray, recordArray.get(i)[2],Boolean.parseBoolean( recordArray.get(i)[3]), Integer.parseInt(recordArray.get(i)[4]),  Integer.parseInt(recordArray.get(i)[5]));
+            else {
+                System.out.println("Not a valid user type!");
+            }
+            records.add(record);
+        }
+        recordListChanged = false;
+    }
     public int addRecord(Record record) throws SQLException {
         String RecordType = record.getClass().getSimpleName();
 
         // if Record is a patient, add an emergency contact as well
         try {
             db.addRecord(record.getPatientEmail(), record.getName(), record.getFrequency(), RecordType);
-            RecordListChanged = true;
+            recordListChanged = true;
         } catch (InputErrorException exception) {
             System.out.println(exception.getMessage());
         }
@@ -38,7 +78,7 @@ public class Doctor extends User {
         try {
 
             db.modifyRecord(currentRecord.getID(), currentRecord.getName(), newRecord.getPatientEmail(), newRecord.getName(), newRecord.getFrequency(), newRecordType);
-            RecordListChanged = true;
+            recordListChanged = true;
         } catch (InputErrorException exception) {
             System.out.println(exception.getMessage());
         }
@@ -47,7 +87,7 @@ public class Doctor extends User {
 
     public int deleteRecord(Record record) throws SQLException, UserNotFoundException {
         db.deleteRecord(record.getID());
-        RecordListChanged = true;
+        recordListChanged = true;
         return 1;
 
     }
