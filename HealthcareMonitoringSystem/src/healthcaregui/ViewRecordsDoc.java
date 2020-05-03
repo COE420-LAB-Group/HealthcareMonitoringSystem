73c6457aa7f91a0c70a0dbd5ced48625f8414dd4
Dubs2310 <b00075201@aus.edu>
2020-05-03 22:51:31 +0400

    hahahaah

--- HealthcareMonitoringSystem/src/healthcaregui/ViewRecordsDoc.java
+++ HealthcareMonitoringSystem/src/healthcaregui/ViewRecordsDoc.java
@@ -44,7 +44,7 @@ public class ViewRecordsDoc extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jLabel1.setText("Patient Records");
